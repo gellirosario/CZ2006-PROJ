b85b954dc7943465f0283f1bbf77c88e6883483b
Edmund Ang <edmund_a@hotmail.com>
2019-10-14 02:20:58 +0800

    View more -> Able to view all news
    ISSUE: unable to click on it and go to webVIew (need to add listener)

--- app/src/main/java/com/cz2006/helloworld/activities/NewsActivity.java
+++ app/src/main/java/com/cz2006/helloworld/activities/NewsActivity.java
@@ -2,22 +2,126 @@ package com.cz2006.helloworld.activities;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.SortedList;
 
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Adapter;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.cz2006.helloworld.R;
+import com.cz2006.helloworld.api.ApiNewsClient;
+import com.cz2006.helloworld.api.ApiNewsInterface;
+import com.cz2006.helloworld.models.Article;
+import com.cz2006.helloworld.models.News;
+import com.cz2006.helloworld.models.Newsutils;
+import com.cz2006.helloworld.models.newsAdapter;
+import com.cz2006.helloworld.models.newsAdapter.OnItemClickListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 
 public class NewsActivity extends AppCompatActivity {
+
+    public static final String API_KEY= "81ebf347fbed47dab591f6ee934b1221";
+    private RecyclerView recyclerView;
+    private RecyclerView.LayoutManager layoutManager;
+    private List<Article> articles = new ArrayList<>();
+    private newsAdapter adapter;
+    private String TAG = NewsActivity.class.getSimpleName();
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news);
+
         setTitle("News");
+
+
+        recyclerView = findViewById(R.id.recycleView);
+        layoutManager = new LinearLayoutManager(NewsActivity.this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setNestedScrollingEnabled(false);
+
+        LoadJson();
+
+
+    }
+//TESTING//
+    private void initListener(){
+
+        adapter.setOnItemClickListener(new OnItemClickListener() {
+            @Override
+            public void onItemClick(View view, int position) {
+
+
+
+            }
+        });
+
+    }
+//TESTING//
+
+
+
+    public void LoadJson(){
+
+        ApiNewsInterface apiNewsInterface = ApiNewsClient.getApiNewsClient().create(ApiNewsInterface.class);
+
+        String country = Newsutils.getCountry();
+
+
+        Call<News> call;
+        call = apiNewsInterface.getNews("q=singapore-environment",API_KEY);
+
+
+        call.enqueue(new Callback<News>() {
+            @Override
+            public void onResponse(Call<News> call, Response<News> response) {
+                if (response.isSuccessful() && response.body().getArticle() != null) {
+
+                    if (!articles.isEmpty()) {
+                        articles.clear();
+                    }
+
+                    articles = response.body().getArticle();
+                    adapter = new newsAdapter(articles, NewsActivity.this);
+                    recyclerView.setAdapter(adapter);
+                    adapter.notifyDataSetChanged();
+
+
+
+                } else {
+                    Toast.makeText(NewsActivity.this, "No Result!", Toast.LENGTH_SHORT).show();
+                }
+            }
+            @Override
+            public void onFailure(Call<News> call, Throwable t) {
+
+            }
+        });
+
+
+
+
+
+
     }
 
     public void setTitle(String title) {
